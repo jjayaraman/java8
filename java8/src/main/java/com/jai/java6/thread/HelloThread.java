3810e7e8da3acf094cd2210e4c5de39b9ae881c2
Jayakumar Jayaraman <india.jai@gmail.com>
2019-03-02 21:47:30 +0000

    Thread trials

---
+++ java8/src/main/java/com/jai/java6/thread/HelloThread.java
@@ -0,0 +1,43 @@
+package com.jai.java6.thread;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+public class HelloThread {
+
+    int priority = Thread.MIN_PRIORITY;
+
+    public static void main(String[] args) {
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(10);
+
+        HelloThread thread = new HelloThread();
+        for (int i = 0; i < 10; i++) {
+            thread.createThread();
+        }
+        try {
+            cyclicBarrier.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (BrokenBarrierException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("end..");
+    }
+
+    private void createThread() {
+        MyThread myThread = new MyThread();
+        myThread.setPriority(priority++);
+        myThread.start();
+    }
+}
+
+
+class MyThread extends Thread {
+
+    @Override
+    public void run() {
+        super.run();
+        System.out.println("Thread running...." + Thread.currentThread().toString());
+    }
+}
