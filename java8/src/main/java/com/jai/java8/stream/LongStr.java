d247f5eae84680bcd5768ae53111a2269bfd6aa8
jjayaraman <india.jai@gmail.com>
2019-03-11 06:38:14 +0000

    java8

--- java8/src/main/java/com/jai/java8/stream/LongStr.java
+++ java8/src/main/java/com/jai/java8/stream/LongStr.java
@@ -5,7 +5,16 @@ import java.util.stream.LongStream;
 public class LongStr {
 
     public static void main(String[] args) {
-        System.out.println(LongStream.of(1l, 2l, 3l, 4, 5).count());
+
+        System.out.println(LongStream.of(1l, 2l, 3l, 4, 5, 10).count());
+
+        System.out.println(LongStream.range(1, 10).count());
+        System.out.println(LongStream.range(1, 10).max());
+        System.out.println(LongStream.range(1, 10).min());
+        System.out.println(LongStream.range(1, 10).average());
+        System.out.println(LongStream.range(1, 1000).summaryStatistics());
+        System.out.println(LongStream.range(1, 10).distinct());
+        System.out.println(LongStream.range(1, 10).findFirst());
 
     }
 }
